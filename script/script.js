9658888f4576b900997d332c3bdec6bcdc259d09
leondasferras <90497102+leondasferras@users.noreply.github.com>
2022-01-08 11:36:41 +0500

    carousel added

--- script/script.js
+++ script/script.js
@@ -1,53 +1,91 @@
 const title = document.querySelector('.carousel-title');
 const description = document.querySelector('.carousel-description');
+const roadIcon = document.querySelector('.section-highway__carousel-road-icon');
 const infoArray = [
   {
     title: "Шоссе",
-    description: "На шоссейном велосипеде можно ездить по асфальту на разных градиентах: будь то горы или равнины. Гонки проходят в командном пелотоне, но тренироваться можно и самостоятельно."
+    description: "На шоссейном велосипеде можно ездить по асфальту на разных градиентах: будь то горы или равнины. Гонки проходят в командном пелотоне, но тренироваться можно и самостоятельно.",
+    icon: "section-highway__carousel-road-icon_bg_highway"
   },
   {
     title: "Грэвел",
-    description: "Грэвел похож на шоссейный велосипед, но конструкция рамы немного отличается, и на нём стоят более широкие покрышки, всё для того чтобы проехать по лёгкому бездорожью."
+    description: "Грэвел похож на шоссейный велосипед, но конструкция рамы немного отличается, и на нём стоят более широкие покрышки, всё для того чтобы проехать по лёгкому бездорожью.",
+    icon: "section-highway__carousel-road-icon_bg_gravel"
   },
   {
     title: "ТТ",
-    description: "ТТ — это велосипед для триатлона или раздельного старта, гооняют на таком велике только по равнинному асфальту, велик очень быстрые и аэродинамичный."
+    description: "ТТ — это велосипед для триатлона или раздельного старта, гооняют на таком велике только по равнинному асфальту, велик очень быстрые и аэродинамичный.",
+    icon: "section-highway__carousel-road-icon_bg_tt"
   }
 ];
 
-// Слайдер для местности.
-function highwaySlider() {
-  let position = 0;
-  let activeElement = 0;
-  let gap = 690;
 
-  const track = document.querySelector('.section-highway__carousel-track');
-  const btnPrev = document.querySelector('.section-highway__carousel-button_move_prev');
-  const btnNext = document.querySelector('.section-highway__carousel-button_move_next');
-
-    btnNext.addEventListener('click', () => {
-      track.style.transform = `translateX(${position - gap}px)`;
-      position -= gap;
-      activeElement += 1;
-      title.textContent = infoArray[activeElement].title;
-      description.textContent = infoArray[activeElement].description;
-    })
-
-
-    btnPrev.addEventListener('click', () => {
-      track.style.transform = `translateX(${position + gap}px)`;
-      position += gap;
-      activeElement -= 1;
-      title.textContent = infoArray[activeElement].title;
-      description.textContent = infoArray[activeElement].description;
-    })
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: ".section-highway__carousel-button_move_next",
+    prevEl: ".section-highway__carousel-button_move_prev",
+  },
+  loop: true,
+  on: {
+    init: function (sw) {
+      changeSliderItemInfo(sw.realIndex);
+    },
+    slideChange: function (sw) {
+      changeSliderItemInfo(sw.realIndex);
+    }
   }
+});
+
+function changeSliderItemInfo(index) {
+  title.textContent = infoArray[index].title;
+  description.textContent = infoArray[index].description;
+  roadIcon.classList = "";
+  roadIcon.classList.add("section-highway__carousel-road-icon");
+  roadIcon.classList.add(infoArray[index].icon);
+}
 
 
-highwaySlider();
+
+
+// Переключение темы
+const themeSwitch = document.querySelector('.footer__switch-container');
+const page = document.querySelector('.page')
+themeSwitch.addEventListener('click', () => {
+  themeSwitch.classList.toggle("footer__switch-container_active")
+  page.classList.toggle("page_dark")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Слайдер для велосипедов.
+/*
 
 function bicyclesSlider() {
   let position = 0;
@@ -115,12 +153,4 @@ function changeActiveDot(newActiveElement) {
 };
 
 bicyclesSlider();
-
-
-// Переключение темы
-const themeSwitch = document.querySelector('.footer__switch-container');
-const page = document.querySelector('.page')
-themeSwitch.addEventListener('click', () => {
-  themeSwitch.classList.toggle("footer__switch-container_active")
-  page.classList.toggle("page_dark")
-})
+*/
